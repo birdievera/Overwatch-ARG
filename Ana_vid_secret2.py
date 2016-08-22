65f3ea0c27e495bb86dfc0cfdb074fbc781bafbd
birdievera <vera.sipicki@gmail.com>
2016-08-22 06:27:47 -0400

    Added second decipher

--- Ana_vid_secret2.py
+++ Ana_vid_secret2.py
@@ -1,24 +1,42 @@
 # numbers from http://pastebin.com/21eE1zLi
 
-num = "65 76 2E 2E 2E 7B 76 20 66 62 72 20 63 7E 72 79 72 20 7B 76 20 7E 79 71 \
-78 65 7A 76 74 7E D4 A4 79 2C 20 63 7E 72 79 72 20 72 7B 20 67 78 73 72 65 2E \
-2E 2E 7B 76 20 66 62 72 20 63 7E 72 79 72 20 7B 76 20 7E 79 71 78 65 7A 76 74 \
-7E 04 A4 79 2C 20 63 7E 72 79 72 20 72 7B 20 67 78 73 72 65 2E 2E 2E 7B 76 20 \
-66 62 72 20 63 7E 72 79 72 20 7B 76 20 7E 79 71 78 65 7A 76 74 7E 04 A4 79 2C \
-20 63 7E 72 79 72 20 72 7B 20 67 78 73 72 65 2E 2E 2E 7B 76 20 66 62 72 20 63 \
-7E 72 79 72 20 7B 76 20 7E 79 71 78 65 7A 76 74 7E 04 A4 79 2C 20 63 7E 72 79 \
+num = "65 76 2E 2E 2E 7B 76 20 66 62 72 20 63 7E 72 79 72 20 7B 76 \
+20 7E 79 71 78 65 7A 76 74 7E D4 A4 79 2C 20 63 7E 72 79 72 \
+20 72 7B 20 67 78 73 72 65 2E 2E 2E 7B 76 20 66 62 72 20 63 \
+7E 72 79 72 20 7B 76 20 7E 79 71 78 65 7A 76 74 7E D4 A4 79 \
+2C 20 63 7E 72 79 72 20 72 7B 20 67 78 73 72 65 2E 2E 2E 7B \
+76 20 66 62 72 20 63 7E 72 79 72 20 7B 76 20 7E 79 71 78 65 \
+7A 76 74 7E D4 A4 79 2C 20 63 7E 72 79 72 20 72 7B 20 67 78 \
+73 72 65 2E 2E 2E 7B 76 20 66 62 72 20 63 7E 72 79 72 20 7B \
+76 20 7E 79 71 78 65 7A 76 74 7E D4 A4 79 2C 20 63 7E 72 79 \
 72 20 72 7B 20 67 78 73 72 65 2E 2E 2E"
-#const = 23
 
-# convert HEX to ASCII
-ascii = [chr(int(n, 16)) for n in num.split(' ')]
+num2 = "2E 2E 2E 7B 76 20 66 62 72 20 63 7E 72 79 72 20 7B 76 20 7E \
+79 71 78 65 7A 76 74 7E D4 A4 79 2C 20 63 7E 72 79 72 20 72 \
+7B 20 67 78 73 72 65 2E 2E 2E 7B 76 20 66 62 72 20 63 7E 72 \
+79 72 20 7B 76 20 7E 79 71 78 65 7A 76 74 7E D4 A4 79 2C 20 \
+63 7E 72 79 72 20 72 7B 20 67 78 73 72 65 2E 2E 2E 7B 76 20 \
+66 62 72 20 63 7E 72 79 72 20 7B 76 20 7E 79 71 78 65 7A 76 \
+74 7E D4 A4 79 2C 20 63 7E 72 79 72 20 72 7B 20 67 78 73 72 \
+65 2E 2E 2E 7B 76 20 66 62 72 20 63 7E 72 79 72 20 7B 76 20 \
+7E 79 71 78 65 7A 76 74 7E D4 A4 79 2C 20 63 7E 72 79 72 20 \
+72 7B 20 67 78 73 72 65 2E 2E 2E 64 78 7A 75"
 
-for const in range(0, 30): 
-    
-    print "const: " + str(const)
-    
+
+const = 23
+# result: la que tiene la informacion; tiene el poder
+# translates to "She who has the information, has the power."
+# last 4 chars of num2 + first 3 chars of num1 = sombra
+
+def xor(code):    
+    # convert HEX to ASCII
+    ascii = [chr(int(n, 16)) for n in code.split(' ')]
+     
     # XOR const
-    string = [ord(n)^const if n != " " else ord(n) for n in ascii]
-    
-    decoded = ''.join((chr(s) for s in string))
-    print (decoded)
+    string = ''.join((chr(ord(n)^const) if n != " " else n for n in ascii))
+        
+    return string
+
+if __name__ == "__main__":
+    print (xor(num))
+    print (xor(num2))
